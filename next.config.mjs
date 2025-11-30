abe5d29fc8db3d5f45f4f72172e422dc4263c1fd
Ariel Jericko Gacilo <bokzgacilo@gmail.com>
2025-12-01 07:45:25 +0800

    nextjs config update 1

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,20 @@ const nextConfig = {
   /* config options here */
   reactCompiler: true,
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "X-Frame-Options", value: "" },
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors https://kasamadigital.com http://localhost:3000;"
+          }
+        ]
+      }
+    ];
+  }
 };
 
 export default nextConfig;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,5 @@
-import { useColorMode } from "@/components/ui/color-mode";
 import { Provider } from "@/components/ui/provider";
 import "@/styles/globals.css";
-import { useTheme } from "next-themes";
-import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
 
