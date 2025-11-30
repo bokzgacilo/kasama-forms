4e7e3d07533e34e27d80e2564a24cf31d1aeea7d
Ariel Jericko Gacilo <bokzgacilo@gmail.com>
2025-12-01 07:49:15 +0800

    nextconfig update 2

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,7 @@ const nextConfig = {
           { key: "X-Frame-Options", value: "" },
           {
             key: "Content-Security-Policy",
-            value: "frame-ancestors https://kasamadigital.com http://localhost:3000;"
+            value: "frame-ancestors *;"
           }
         ]
       }
